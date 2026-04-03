204c46e5cd150b57503eb42a3d5cc1463dd8ae07
kismokesz <124524542+kismokesz@users.noreply.github.com>
2026-04-03 14:44:39 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,18 @@
 import os
 import discord
 from discord.ext import commands
-from dotenv import load_dotenv
 
-load_dotenv()  # Lokálisan kell, Railway-n a környezeti változók automatikusak
+intents = discord.Intents.default()
+intents.message_content = True
 
-TOKEN = os.getenv("DISCORD_TOKEN")
-bot = commands.Bot(command_prefix="!")
-
-@bot.event
-async def on_ready():
-    print(f"{bot.user} elindult!")
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send("Pong!")
+    await ctx.send("Pong! 🏓")
 
-bot.run(TOKEN)
+@bot.event
+async def on_ready():
+    print(f'Bejelentkezve mint {bot.user}')
+
+bot.run(os.getenv("DISCORD_TOKEN"))
