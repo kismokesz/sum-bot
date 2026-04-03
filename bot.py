a6acf8f74f831eb6bddd26b6cfce7416620208f5
kismokesz <124524542+kismokesz@users.noreply.github.com>
2026-04-03 15:50:11 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,23 +5,23 @@ from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.message_content = True
-intents.messages = True  # Kell, hogy olvashassa az üzeneteket
+intents.messages = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-# Ping parancs teszteléshez
 @bot.command()
 async def ping(ctx):
     await ctx.send("Pong! 🏓")
 
-# Fidesz Berenc parancs
 @bot.command(name="fidesz_berenc")
 async def fidesz_berenc(ctx):
-    # Lekérjük a csatorna összes üzenetét (max 1000)
     szamok = []
     async for msg in ctx.channel.history(limit=1000):
-        # Keresés: minden számot kiválasztunk az üzenetből
-        szamok += [int(n) for n in re.findall(r'\d+', msg.content)]
+        # Keresés minden számra (szóköz vagy vessző lehet)
+        talalatok = re.findall(r'\d[\d ,]*\d|\d', msg.content)
+        for t in talalatok:
+            # Vessző és szóköz eltávolítása
+            szamok.append(int(t.replace(",", "").replace(" ", "")))
     
     if szamok:
         osszeg = sum(szamok)
@@ -33,6 +33,5 @@ async def fidesz_berenc(ctx):
 async def on_ready():
     print(f'Bejelentkezve mint {bot.user}')
 
-# Token Railway environment variable-ból
 token = os.getenv("DISCORD_TOKEN")
 bot.run(token)
